04c385668b984dc78a45ab270a1069e975f1e9bd
bangongzi <zhbiubiu@gmail.com>
2020-12-30 20:48:21 +0800

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,81 +1,55 @@
+package Algorithm;
 import java.util.Random;
 
 /**
- * Created by per on 2018/1/27.
+ *  快速排序的思想是分而治之。每次排好一个位置，使这个元素左边的位置都不大于这个元素，使这个元素右边的元素都不小于这个元素。
+ *  迭代的对这个位置的左边和右边进行排序，即可得到最终排序结果。时间复杂度O(nlog(n))，空间复杂度O(n)
+ *  如果数组有序程度较高，用快排可能会有O(n^2)这样高时间复杂度风险。
+ *  在这里我们采用随机打乱的方式避免，而实际上应用中，可预先判断有序程度，如果有序程度高，可结合堆排序进来(如Java Arrays.sort()中做法)。
+ *  这里的partition函数是从左到右单个指针遍历，而非左右两个指针一起遍历。这么做方便记忆、手写代码，且似乎也没有丢失什么性能。
  */
-/*
- *快速排序的基本逻辑是，随机打乱待排序序列（这主要是为了避免O(n2)的最糟糕情况），在排好一个位置，使这个元素左边的位置都不大于这个元素，使这个元素右边的元素都不小于这个元素。
- *并在选定位置的左边与右边再次进行迭代操作
- */
+public class QuickSort {
+    // 按升序排序
+    public static void sortSingle(int[] nums) {
+        if (nums == null || nums.length == 0) return;
 
-public class Draft {
-    public void quickSortNoReturn(int[] nums) {
-        // 自己想的方法，直接在sortHelp里边实现快排序迭代，没有返回值
-        int len = nums.length;
-        if(len <= 1) return;
-        sortHelp(nums, 0, len - 1);
+        sortSingle(nums, 0, nums.length - 1);
     }
 
-    public void quickSortNormal(int[] nums, int left, int right) {
-        // 这是《剑指offer》上的方法，一方面避免了相同元素带来的多次交换，一方面partitionSword（）函数可以用于得到无序序列里边第k大的值
-        if(nums == null || left>= right) return;
-        int index = partitionSword(nums, left, right);
-        quickSortNormal(nums, left, index - 1);
-        quickSortNormal(nums, index + 1, right);
+    private static void sortSingle(int[] nums, int lo, int hi) {
+        if (lo >= hi) return;
+
+        int index = partitionSingle(nums, lo, hi);
+        sortSingle(nums, lo, index - 1);
+        sortSingle(nums, index + 1, hi);
     }
 
-    public int partitionSword(int[] nums, int left, int right) {
-        if(left >= right) return left;
+    private static int partitionSingle(int[] nums, int lo, int hi) {
+        if(lo >= hi) return lo;
+
         Random random = new Random();
-        int index = random.nextInt(right - left + 1) + left; // index is a random number in [left, right]
-        swap(nums, index, right); // put the 'to be in-position' value in the right for a while
-        int val = nums[right];
-        int small = left - 1; // where the smaller number stay
-        for(index = left; index < right; index++) {
+        int index = random.nextInt(hi - lo + 1) + lo;
+        swap(nums, index, lo);
+
+        int val = nums[lo];
+        int smallIndex = lo; // where the smaller number stay
+
+        // 把比给定元素小的，都放到左边
+        for(index = lo + 1; index <= hi; index++) {
             if(nums[index] < val) {
-                ++small;
-                if(small < index) {
-                    swap(nums, small, index);
+                if(++smallIndex < index) {
+                    swap(nums, smallIndex, index);
                 }
             }
         }
-        ++small;
-        swap(nums, small, right);
-        return small;
+
+        swap(nums, smallIndex, lo);
+        return smallIndex;
     }
 
-    public void sortHelp(int[] nums, int start, int end) {
-        if(start >= end) return;
-        int val = nums[start];
-        int i = start, j = end;
-        while(i <= j) {
-            if(nums[i] <= val) {
-                i++;
-            } else if(nums[j] >= val) {
-                j--;
-            } else {
-                swap(nums, i, j);
-            }
-        }
-        swap(nums, start, j);
-        sortHelp(nums, start, j-1);
-        sortHelp(nums, i, end);
-    }
-
-    public void swap(int[] nums, int i, int j) {
+    private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
-
-    public static void main(String args[]) {
-        int[] nums = {2, 1, 3, 6, 12, 3, 7, 21, 7};
-        // int[] nums = {2, 1};
-        // int[] nums = {2};
-        Draft test = new Draft();
-        test.quickSortNormal(nums, 0, nums.length - 1);
-        for(int i: nums) {
-            System.out.println(i);
-        }
     }
 }
